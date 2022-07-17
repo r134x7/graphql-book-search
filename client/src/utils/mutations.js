ae1d1a8b5c34ec6cd63bf39bc769663f38fd6987
r134x7 <r134x7@gmx.com>
2022-07-17 18:48:54 +1000

    changed query and mutation input/output from server testing

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,22 +2,29 @@ import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String!) {
-        loginUser(email: $email, password: $password) {
-            email
-            password
-        }
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
 `;
 
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
-            username
-            email
-            password
+            token
+            user {
+                _id
+                username
+            }
         }
     }
 `;
+
+// I assume I am going to need to the user's ID when saving or removing a book...
 
 export const SAVE_BOOK = gql`
     mutation saveBook($input: saveBookInput) {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,13 +1,22 @@
 import { gql } from "@apollo/client";
 
+// assuming this is all the data to retrieve when the user views their profile and saved books
+
 export const GET_ME = gql`
     query me {
         me {
-            _id
-            username
-            email
-            bookCount
-            savedBooks
-        },
-    },
+          _id
+          username
+          email
+          bookCount
+            savedBooks {
+              bookId
+              authors
+              image
+              title
+              description
+              link
+            }
+        }
+    }
 `;
